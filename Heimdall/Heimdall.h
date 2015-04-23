28ee7148758bd3fb433b75d9a9a7740417422a79
Henri Normak <henri.normak@gmail.com>
2015-04-23 11:07:41 +0100

    Initial Commit

---
+++ Heimdall/Heimdall.h
@@ -0,0 +1,19 @@
+//
+//  Heimdall.h
+//  Heimdall
+//
+//  Created by Henri Normak on 23/04/15.
+//  Copyright (c) 2015 Henri Normak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Heimdall.
+FOUNDATION_EXPORT double HeimdallVersionNumber;
+
+//! Project version string for Heimdall.
+FOUNDATION_EXPORT const unsigned char HeimdallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Heimdall/PublicHeader.h>
+
+
